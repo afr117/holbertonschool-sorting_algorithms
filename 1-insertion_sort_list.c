8dc1a10171207bd490769533197f4fc85751b083
afr117 <6666@holbertonstudents.com>
2023-08-20 13:46:36 -0700

    1..100

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -2,7 +2,7 @@
 
 /**
  * insertion_sort_list - Sorts a doubly linked list of integers in
- * ascending order using the Insertion sort algorithm
+ *                      ascending order using the Insertion sort algorithm
  * @list: Pointer to a pointer to the head of the doubly linked list
  */
 void insertion_sort_list(listint_t **list)
