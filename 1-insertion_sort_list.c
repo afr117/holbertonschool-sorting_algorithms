419e618fb28ae8986f1e658647eeb14dc70c7bd8
afr117 <6666@holbertonstudents.com>
2023-08-21 15:14:27 -0700

    1..100

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -15,33 +15,39 @@ void insertion_sort_list(listint_t **list)
         return;
 
     listint_t *current = (*list)->next;
+
     while (current != NULL)
     {
-        listint_t *next_node = current->next;
-        listint_t *temp = current;
-        listint_t *prev = temp->prev;
-
-        while (prev != NULL && prev->n > temp->n)
-        {
-            prev->next = temp->next;
-            if (temp->next != NULL)
-                temp->next->prev = prev;
-
-            temp->next = prev;
-            temp->prev = prev->prev;
-
-            if (prev->prev != NULL)
-                prev->prev->next = temp;
-            else
-                *list = temp;
-
-            prev->prev = temp;
-
-            print_list(*list);
-            prev = temp->prev;
-        }
-
-        current = next_node;
+        insertion_sort_step(list, &current);
     }
+}
+
+void insertion_sort_step(listint_t **list, listint_t **current)
+{
+    listint_t *next_node = (*current)->next;
+    listint_t *temp = *current;
+    listint_t *prev = temp->prev;
+
+    while (prev != NULL && prev->n > temp->n)
+    {
+        prev->next = temp->next;
+        if (temp->next != NULL)
+            temp->next->prev = prev;
+
+        temp->next = prev;
+        temp->prev = prev->prev;
+
+        if (prev->prev != NULL)
+            prev->prev->next = temp;
+        else
+            *list = temp;
+
+        prev->prev = temp;
+
+        print_list(*list);
+        prev = temp->prev;
+    }
+
+    *current = next_node;
 }
 
