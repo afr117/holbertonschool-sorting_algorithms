d596b15735b5b249796508ac3b2c4dd82dbcca3f
afr117 <6666@holbertonstudents.com>
2023-08-21 15:28:17 -0700

    1..100

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,46 +1,40 @@
 #include "sort.h"
 
-/**
- * insertion_sort_list - Sorts a doubly linked list of integers in ascending
- *                      order using the insertion sort algorithm.
- * @list: A pointer to a pointer to the head of the list.
- *
- * Description: This function implements the insertion sort algorithm
- *              on a doubly linked list. It rearranges the nodes in the
- *              list to sort them in ascending order.
- */
 void insertion_sort_list(listint_t **list)
 {
     if (list == NULL || *list == NULL || (*list)->next == NULL)
         return;
 
     listint_t *current = (*list)->next;
-    listint_t *next_node, *temp, *prev;
 
     while (current != NULL)
     {
-        next_node = current->next;
-        temp = current;
-        prev = temp->prev;
+        listint_t *next_node = current->next;
 
-        while (prev != NULL && prev->n > temp->n)
+        if (current->prev != NULL && current->prev->n > current->n)
         {
-            prev->next = temp->next;
-            if (temp->next != NULL)
-                temp->next->prev = prev;
+            listint_t *temp = current;
 
-            temp->next = prev;
-            temp->prev = prev->prev;
+            while (temp->prev != NULL && temp->prev->n > temp->n)
+            {
+                listint_t *prev = temp->prev;
 
-            if (prev->prev != NULL)
-                prev->prev->next = temp;
-            else
-                *list = temp;
+                prev->next = temp->next;
+                if (temp->next != NULL)
+                    temp->next->prev = prev;
 
-            prev->prev = temp;
+                temp->next = prev;
+                temp->prev = prev->prev;
 
-            print_list(*list);
-            prev = temp->prev;
+                if (prev->prev != NULL)
+                    prev->prev->next = temp;
+                else
+                    *list = temp;
+
+                prev->prev = temp;
+
+                temp = prev->prev;
+            }
         }
 
         current = next_node;
