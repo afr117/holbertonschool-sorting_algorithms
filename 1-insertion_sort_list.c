8e274261f7658d8e7498092543f0c36d71e67ef5
afr117 <6666@holbertonstudents.com>
2023-08-21 14:51:39 -0700

    1..100

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -4,7 +4,10 @@
  * insertion_sort_list - Sorts a doubly linked list of integers in ascending
  *                      order using the insertion sort algorithm.
  * @list: A pointer to a pointer to the head of the list.
- * Return: A pointer to the sorted list.
+ *
+ * Description: This function implements the insertion sort algorithm
+ *              on a doubly linked list. It rearranges the nodes in the
+ *              list to sort them in ascending order.
  */
 
     listint_t *current;
@@ -14,37 +17,36 @@
 
 void insertion_sort_list(listint_t **list)
 {
-    if (list == NULL || *list == NULL || (*list)->next == NULL)
-        return;
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
+	return;
 
-    current = (*list)->next;
-    while (current != NULL)
-    {
-        next_node = current->next;
-        temp = current;
-        prev = temp->prev;
+	current = (*list)->next;
+	while (current != NULL)
+	{
+	next_node = current->next;
+	temp = current;
+	prev = temp->prev;
 
-        while (prev != NULL && prev->n > temp->n)
-        {
-            prev->next = temp->next;
-            if (temp->next != NULL)
-                temp->next->prev = prev;
+	while (prev != NULL && prev->n > temp->n)
+	{
+	prev->next = temp->next;
+	if (temp->next != NULL)
+	temp->next->prev = prev;
 
-            temp->next = prev;
-            temp->prev = prev->prev;
+	temp->next = prev;
+	temp->prev = prev->prev;
 
-            if (prev->prev != NULL)
-                prev->prev->next = temp;
-            else
-                *list = temp;
+	if (prev->prev != NULL)
+	prev->prev->next = temp;
+	else
+	*list = temp;
 
-            prev->prev = temp;
+	prev->prev = temp;
 
-            print_list(*list);
-            prev = temp->prev;
-        }
-
-        current = next_node;
-    }
+	print_list(*list);
+	prev = temp->prev;
+}
+	current = next_node;
+}
 }
 
