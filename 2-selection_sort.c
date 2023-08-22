3a9e9ead5f580064da64c8d24b9f1ae7c1e6a156
afr117 <6666@holbertonstudents.com>
2023-08-21 22:40:38 -0700

    1..100

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -8,6 +8,8 @@
  * @array: The array to be sorted.
  * @size: The size of the array.
  */
+
+
 void selection_sort(int *array, size_t size)
 {
     size_t i, j, min_idx;
@@ -19,21 +21,21 @@ void selection_sort(int *array, size_t size)
     {
         min_idx = i;
 
-        // Find the index of the minimum element in the unsorted portion
+        /* Find the index of the minimum element in the unsorted portion */
         for (j = i + 1; j < size; j++)
         {
             if (array[j] < array[min_idx])
                 min_idx = j;
         }
 
-        // Swap the minimum element with the current element if necessary
+        /* Swap the minimum element with the current element if necessary */
         if (min_idx != i)
         {
             int temp = array[i];
             array[i] = array[min_idx];
             array[min_idx] = temp;
 
-            // Print the array after each swap
+            /* Print the array after each swap */
             print_array(array, size);
         }
     }
