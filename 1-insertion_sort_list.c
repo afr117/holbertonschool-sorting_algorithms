6c7a717c9e5576744ab92ff7b443fed89414ddd0
afr117 <6666@holbertonstudents.com>
2023-08-21 19:16:38 -0700

    1..100

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,14 +1,25 @@
 #include "sort.h"
 
+/**
+ * insertion_sort_list - Sorts a doubly linked list of integers in ascending
+ *                      order using the insertion sort algorithm.
+ * @list: A pointer to a pointer to the head of the list.
+ *
+ * Description: This function implements the insertion sort algorithm
+ *              on a doubly linked list. It rearranges the nodes in the
+ *              list to sort them in ascending order.
+ */
 void insertion_sort_list(listint_t **list)
 {
-    listint_t *current, *next_node, *temp, *prev;
-
     if (list == NULL || *list == NULL || (*list)->next == NULL)
         return;
 
-    current = (*list)->next;
+    listint_t *current;
+    listint_t *next_node;
+    listint_t *temp;
+    listint_t *prev;
 
+    current = (*list)->next;
     while (current != NULL)
     {
         next_node = current->next;
@@ -21,15 +32,17 @@ void insertion_sort_list(listint_t **list)
             if (temp->next != NULL)
                 temp->next->prev = prev;
 
+            temp->next = prev;
+            temp->prev = prev->prev;
+
             if (prev->prev != NULL)
                 prev->prev->next = temp;
             else
                 *list = temp;
 
-            temp->next = prev;
-            temp->prev = prev->prev;
             prev->prev = temp;
 
+            print_list(*list);
             prev = temp->prev;
         }
 
