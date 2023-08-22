7ce45e0e4deddf6f8fb783b2cddbcfe85fa50e60
afr117 <6666@holbertonstudents.com>
2023-08-21 22:47:46 -0700

    1..100

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -2,8 +2,7 @@
 #include "sort.h"
 
 /**
- * selection_sort - Sorts an array of integers in ascending order
- *                  using the Selection sort algorithm.
+ * selection_sort - Sorts array of integers, ascending order using Selection sort algorithm.
  *
  * @array: The array to be sorted.
  * @size: The size of the array.
@@ -12,32 +11,31 @@
 
 void selection_sort(int *array, size_t size)
 {
-    size_t i, j, min_idx;
+	size_t i, j, min_idx;
 
-    if (array == NULL || size < 2)
-        return;
+	if (array == NULL || size < 2)
+	return;
 
-    for (i = 0; i < size - 1; i++)
-    {
-        min_idx = i;
+	for (i = 0; i < size - 1; i++)
+	{
+	min_idx = i;
 
-        /* Find the index of the minimum element in the unsorted portion */
-        for (j = i + 1; j < size; j++)
-        {
-            if (array[j] < array[min_idx])
-                min_idx = j;
-        }
-
-        /* Swap the minimum element with the current element if necessary */
-        if (min_idx != i)
-        {
-            int temp = array[i];
-            array[i] = array[min_idx];
-            array[min_idx] = temp;
-
-            /* Print the array after each swap */
-            print_array(array, size);
-        }
-    }
+	/* Find the index of the minimum element in the unsorted portion */
+	for (j = i + 1; j < size; j++)
+	{
+	if (array[j] < array[min_idx])
+	min_idx = j;
 }
 
+/* Swap the minimum element with the current element if necessary */
+if (min_idx != i)
+{
+	int temp = array[i];
+	array[i] = array[min_idx];
+	array[min_idx] = temp;
+
+/* Print the array after each swap */
+print_array(array, size);
+}
+}
+}
